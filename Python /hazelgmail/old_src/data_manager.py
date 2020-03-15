5e9ce7479b86cffb79884be5a2204794e7a0ebba
elaocak <49361580+darkbluepebble@users.noreply.github.com>
2020-03-15 23:09:40 +0300

    Add files via upload

---
+++ Python /hazelgmail/old_src/data_manager.py
@@ -0,0 +1,38 @@
+import csv
+import datetime
+import shutil
+import os
+from tempfile import NamedTemporaryFile
+
+
+#file_item_path=os.path.join(os.getcwd(),"data.csv")
+file_item_path=os.path.join(os.path.dirname(__file__),"data.csv")
+
+
+def read_data(user_id=None, email=None) :
+	filename=file_item_path
+	print(filename)
+	with open(filename,"r") as csvfile:
+		reader=csv.DictReader(csvfile)
+		items=[]
+		unknown_user_id=None
+		unknown_email=None
+
+		for row in reader:
+			if user_id is not None :
+				if int(user_id) == int(row.get("id")):
+					return row
+				else :
+					unknown_user_id=user_id
+
+			if email is not None :
+				if email == row.get("email") :
+					return row
+				else :
+					unknown_email=email
+
+		if unknown_user_id is not None :
+			print("User id {userid} not found".format(userid=user_id))
+		if unknown_email is  not  None :
+			print("User email {email} is not found".format(email=email))
+	return None	
