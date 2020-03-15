91ad24447dd3027f9d3c64895afa97e93e2556b4
elaocak <49361580+darkbluepebble@users.noreply.github.com>
2020-03-15 23:08:00 +0300

    Add files via upload

---
+++ Python /hazelgmail/utils/templates.py
@@ -0,0 +1,12 @@
+import os 
+def get_template_path(path):
+	file_path = os.path.join(os.path.dirname(__file__),path  ) 
+	if not os.path.join(os.getcwd(),path) :
+		raise Exception("this is error path %s"%(file_path) )
+	return file_path
+def get_template(path):
+	file_path=get_template_path(path)
+	return open(file_path).read()
+def render_context(template_string,context):
+	return template_string.format(**context)
+
